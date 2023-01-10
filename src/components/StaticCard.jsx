0a98aefb9861469099384084d1f6b58254429958
baharsah <me@baharsah.my.id>
2023-01-10 15:00:49 +0700

    refactor color and positioning

--- src/components/StaticCard.jsx
+++ src/components/StaticCard.jsx
@@ -3,11 +3,11 @@ import Img1 from '../assets/OG_Image_ARSB_66cefac2a6.jpg'
 
 function StaticCard() {
   return (
-    <div className='bg-gradient-to-r from-pink-500 to-yellow-500 py-16 px-8'>
+    <div className='bg-gradient-to-r from-pink-500 to-red-400 py-16 px-16'>
     <div className="flex justify-between w-full h-full p-3">
    
-            <div className='py-3'>
-            <p className=' font-bold  text-white w-3/5 my-16 text-5xl'>
+            <div className='py-1'>
+            <p className=' font-bold  text-white w-3/5 my-8 text-5xl'>
                Hello World! 
                <br /> 
             </p>
@@ -17,7 +17,7 @@ function StaticCard() {
             
             <div className='w-1/4 h-1/4 py-5 rounded relative'>
                 <img  className='rounded-lg absolute border-8' src={Img1}></img>
-            <img  className='rounded-lg absolute translate-y-7 -translate-x-7 border-8' src={Img1}></img>
+            <img  className='rounded-lg absolute -translate-y-7 translate-x-7 border-8' src={Img1}></img>
 
             </div>
 
