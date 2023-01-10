744e6c7a5a9f8e13ae9e68775ec8e946ae10cd98
baharsah <me@baharsah.my.id>
2023-01-10 14:55:23 +0700

    navbar

--- src/components/StaticCard.jsx
+++ src/components/StaticCard.jsx
@@ -3,21 +3,26 @@ import Img1 from '../assets/OG_Image_ARSB_66cefac2a6.jpg'
 
 function StaticCard() {
   return (
-    <div className="flex justify-between bg-gradient-to-r from-pink-500 to-pink-600 w-full h-full p-2">
+    <div className='bg-gradient-to-r from-pink-500 to-yellow-500 py-16 px-8'>
+    <div className="flex justify-between w-full h-full p-3">
    
-            <div className='px-16 py-1'>
+            <div className='py-3'>
             <p className=' font-bold  text-white w-3/5 my-16 text-5xl'>
-               Hello Nigga! 
+               Hello World! 
                <br /> 
             </p>
 
                <p className='text-normal text-4xl text-white'>you wanna to go home? Check our services!</p>
             </div>
             
-            <div className='w-72 h-72 rounded'>
-                <img  className='rounded-lg' src={Img1}></img>
+            <div className='w-1/4 h-1/4 py-5 rounded relative'>
+                <img  className='rounded-lg absolute border-8' src={Img1}></img>
+            <img  className='rounded-lg absolute translate-y-7 -translate-x-7 border-8' src={Img1}></img>
+
             </div>
+
   
+    </div>
     </div>
   )
 }
